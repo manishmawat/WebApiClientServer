7d6a0838083cc5c1bbe481e468052ee36a24f33f
MKM <manish.mawat@gmail.com>
2022-01-07 00:43:22 -0500

    Add project files.

---
+++ UserWebClient/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UserWebClient.Models;
+
+namespace UserWebClient.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<IActionResult> IndexAsync()
+    {
+        ViewData["Message"] = "Hello from webfrontend";
+        HttpClient httpClient = new HttpClient();
+        var response = await httpClient.GetAsync("http://userwebserver/WeatherForecast");
+        var jsonString = await response.Content.ReadAsStringAsync();
+        var viewModel = JsonConvert.DeserializeObject<List<WeatherForecast>>(jsonString);
+        return View(viewModel);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
